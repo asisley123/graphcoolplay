f770ef2584a9ee95ed2dc97e4efa8c90c04403dd
Nikolas Burk <nikolas.burk@gmail.com>
2017-02-20 11:17:15 +0100

    deleted auth0 config file

--- exponent-auth0/main.js
+++ exponent-auth0/main.js
@@ -7,7 +7,6 @@ import PostListView from './src/PostListView'
 import PostDetailView from './src/PostDetailView'
 import {createRouter, NavigationProvider, StackNavigation} from '@exponent/ex-navigation'
 
-
 // refer to the README that explains how to set the following vaqriables
 export const auth0_client_id = 'uLSnQEYIghkTAzRwst7bsm0SucHulkXV'
 export const authorize_url = 'https://nikolasburk.eu.auth0.com/authorize'
